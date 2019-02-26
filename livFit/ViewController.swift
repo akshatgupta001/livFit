5471ea3c5acfbc89d53f3fdc47efe455e8bc20ed
Akshat Gupta <akshatgupta001@live.com>
2019-02-27 02:47:42 +0530

    Initial Commit

---
+++ livFit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  livFit
+//
+//  Created by Akshat Gupta on 27/02/19.
+//  Copyright © 2019 coded. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
